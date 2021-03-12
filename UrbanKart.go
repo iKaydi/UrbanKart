dd9d4b1cf901935461b8d5d8a83a18a6ed9d3167
Tanveer Akram <tanveerakramrockz@gmail.com>
2021-03-12 21:43:24 +0530

    Full code finished

--- UrbanKart.go
+++ UrbanKart.go
@@ -6,6 +6,7 @@ type User struct { //Struct User to store mobile and address
 	Mobile  int
 	Address string
 	Trolley Cart
+	Total   int
 }
 
 type List struct { //Struct List to store array of Users
@@ -19,8 +20,8 @@ type Product struct {
 }
 
 type Cart struct {
-	Kart  []Product
-	Total int
+	Kart []Product
+	//Total int
 }
 
 var P1 Product = Product{15000, 1, "TV"}
@@ -70,7 +71,7 @@ func GetQuan() int {
 	return i
 }
 
-func (L *List) GetProducts(quan int, cust_id int) {
+func (L *List) PlaceOrder(quan int, cust_id int) {
 	var ch, k int
 	//L.U[cust_id].Trolley.Kart[cust_id] = Product{}
 	//fmt.Printf("%v", Bag)
@@ -87,6 +88,7 @@ func (L *List) GetProducts(quan int, cust_id int) {
 			TempProduct = Product{BP, BIN, BN}
 			L.U[cust_id].Trolley.Kart = append(L.U[cust_id].Trolley.Kart, TempProduct)
 			fmt.Printf("%v", L.U[cust_id].Trolley)
+			L.U[cust_id].Total = L.U[cust_id].Total + 15000
 			//L.U[cust_id].Trolley.Kart = append(L.U[cust_id].Trolley.Kart, TempProduct)
 			//fmt.Printf("%v", L.U[cust_id])
 			//Bag[k].Productname = "TV"
@@ -96,7 +98,7 @@ func (L *List) GetProducts(quan int, cust_id int) {
 			//L.U[cust_id].Trolley.Kart[z].Price = 15000
 			//L.U[cust_id].Trolley.Kart[z].Itemno = 1
 			//L.U[cust_id].Trolley.Kart[z].Productname = "TV"
-			L.U[cust_id].Trolley.Total = 15000 + L.U[cust_id].Trolley.Total
+			//MOST RECENT CORRECT REFER FOR OTHER IF//////////L.U[cust_id].Trolley.Total = 15000 + L.U[cust_id].Trolley.Total
 			//fmt.Printf("%v", TempProduct)
 			//TempProduct = Product{L.U[cust_id].Trolley.Kart[z].Price, L.U[cust_id].Trolley.Kart[z].Itemno, L.U[cust_id].Trolley.Kart[z].Productname}
 			//L.U[cust_id].Trolley.Kart[k] = append(L.U[cust_id].Trolley.Kart[k],TempProduct)
@@ -112,7 +114,8 @@ func (L *List) GetProducts(quan int, cust_id int) {
 			TempProduct = Product{BP, BIN, BN}
 			//L.U[cust_id].Trolley.Kart = append(Bag, TempProduct)
 			L.U[cust_id].Trolley.Kart = append(L.U[cust_id].Trolley.Kart, TempProduct)
-			L.U[cust_id].Trolley.Total = 25000 + L.U[cust_id].Trolley.Total
+			L.U[cust_id].Total = L.U[cust_id].Total + 25000
+			//L.U[cust_id].Trolley.Total = 25000 + L.U[cust_id].Trolley.Total
 			//fmt.Printf("%v", TempProduct)
 			//TempProduct = Product{Bag[k].Itemno, Bag[k].Price, Bag[k].Productname}
 			//fmt.Printf("%v", TempProduct)
@@ -134,7 +137,8 @@ func (L *List) GetProducts(quan int, cust_id int) {
 			//fmt.Printf("%v", TempProduct)
 			//L.U[cust_id].Trolley.Kart = append(Bag, TempProduct)
 			L.U[cust_id].Trolley.Kart = append(L.U[cust_id].Trolley.Kart, TempProduct)
-			L.U[cust_id].Trolley.Total = 7500 + L.U[cust_id].Trolley.Total
+			L.U[cust_id].Total = 7500 + L.U[cust_id].Total
+			//L.U[cust_id].Trolley.Total = 7500 + L.U[cust_id].Trolley.Total
 			//	TempProduct = Product{Bag[k].Itemno, Bag[k].Price, Bag[k].Productname}
 			//		fmt.Printf("%v", TempProduct)
 			//L.U[cust_id].Trolley.Kart[z].Price = 7500
@@ -148,6 +152,43 @@ func (L *List) GetProducts(quan int, cust_id int) {
 	fmt.Printf("%v", L.U)
 }
 
+func (L *List) ChangeOrder(cust_id int) {
+	for true {
+		fmt.Printf("What would you like to remove?\nPress 99 to exit\n")
+		fmt.Printf("%v", L.U[cust_id].Trolley) //Prints the array of structure Product
+		//fmt.Printf("%v", L.U[cust_id].Trolley.Kart[0]) // Prints 0th array of structure Product
+		var a int
+		fmt.Scanln(&a)
+		if a == 99 {
+			break
+		} else {
+			L.U[cust_id].Total = L.U[cust_id].Total - L.U[cust_id].Trolley.Kart[a].Price
+			L.U[cust_id].Trolley.Kart[a] = Product{0, 0, ""}
+			fmt.Printf("%v", L.U[cust_id].Trolley)
+		}
+	}
+}
+
+func (L *List) CancelOrder(cust_id int) {
+	for k := range L.U {
+		if k == cust_id {
+			L.U[cust_id].Trolley = Cart{}
+			L.U[cust_id].Total = 0
+			fmt.Printf("%v", L.U)
+		}
+	}
+}
+
+func (L *List) DisplayOrder(cust_id int) {
+	for k := range L.U {
+		if k == cust_id {
+			fmt.Println("Products you have ordered are")
+			fmt.Println(L.U[cust_id].Trolley)
+			fmt.Println("Total :", L.U[cust_id].Total)
+		}
+	}
+}
+
 func main() {
 
 	list1 := List{}
@@ -159,7 +200,7 @@ func main() {
 		fmt.Scanln(&ch)
 		if ch == 1 {
 			n, s, cart1 = ScanUser()
-			user1 := User{n, s, cart1}
+			user1 := User{n, s, cart1, 0}
 			list1.AddUsers(user1)
 			fmt.Printf("%d %v \n", i, list1)
 			i++
@@ -170,12 +211,34 @@ func main() {
 		if ch == 3 {
 			cust_id = WhichUSer() //Customer number
 			quan = GetQuan()      //Quantity
-			list1.GetProducts(quan, cust_id)
+			list1.PlaceOrder(quan, cust_id)
 
 			//fmt.Printf("%v", list1.U[cust_id]) //succesfull print
 			//fmt.Println(quan)                  //succesful print
 			//Prod()
 
+		}
+		if ch == 4 {
+			cust_id = WhichUSer()
+			quan = GetQuan()
+			fmt.Println("1.Add\n2.Remove")
+			var a int
+			fmt.Scanln(&a)
+			if a == 1 {
+				list1.PlaceOrder(quan, cust_id)
+			}
+			if a == 2 {
+				list1.ChangeOrder(cust_id)
+			}
+		}
+		if ch == 5 {
+			cust_id = WhichUSer()
+			list1.CancelOrder(cust_id)
+		}
+
+		if ch == 6 {
+			cust_id = WhichUSer()
+			list1.DisplayOrder(cust_id)
 		}
 	}
 }
